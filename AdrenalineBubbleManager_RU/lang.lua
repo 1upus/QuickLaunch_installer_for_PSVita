9ff9d08c05cb0bc905343d754b7d3a3860320ea7
manwithnoface <anbarsukov@gmail.com>
2017-10-21 15:17:50 +0300

    Добавлен русификатор для ABM 4.01
    Добавлен русификатор для AdrenalineBubbleManager 4.01

---
+++ AdrenalineBubbleManager_RU/lang.lua
@@ -0,0 +1,58 @@
+--Texts (only text in " ")
+strings = {
+
+--script.lua
+unsafe =		"Необходимо активировать UNSAFE MODE для Homebrew!!!",
+adrinst =		"Плагин AdrBubbleBooter установлен. \nТребуется выполнить перезагрузку PS Vita... :)",
+notadr =		"Adrenaline v6 не обнаружен...",
+
+--callbacks.lua
+instbb =		"Создание пузыря: ",
+file =			"Файл: ",
+
+--bubbles.lua
+btitle =		"НАСТРОЙКИ DATA/BOOT.INF",
+count =			"Всего: ",
+
+--osk
+titleosk =		"Название пузыря",
+putnameosk =	"Введите имя",
+--osk
+
+convert =		"Конвертирование ресурсов...",
+errinst =		"В процессе установки произошла ошибка.",
+unk =			"UNK",
+
+marks =			"Select/Start (Выделить/Выделить все)",
+uninstall =		"Удалить",
+editboot =		"Редактировать boot.inf",
+back =			"Назад",
+toggle =		"Изменить значение",
+doneedit =		"Применить",
+notbubbles =	"Данные не найдены.",
+createbb =		"Создайте пузырь и повторите попытку.",
+togoback =		"Назад",
+uninstallbb =	"Будет удалено пузырей: ",
+
+--scan.lua
+scantitle =		"Доступные игры [ISO/CSO/PBP]",
+
+--debug
+loadciso =		"Загрузка C/ISO",
+loadpbp =		"Загрузка PBP",
+--debug
+
+defcolor =		"Фоновый цвет пузыря",
+bbcolor =		"Фоновый цвет пузыря",
+pics =			"Просмотр оформления",
+markgame =		"Выделить/снять выделение",
+bsettings =		"Настройки пузырей",
+
+notpsp =		"Не найдено ни одной PSP игры...",
+installgames =	"Поместите образы iso/cso в папку pspemu/ISO.",
+installgames2 =	"Игры формата eboot.pbp - в pspemu/PSP/GAME/(MYGAME)",
+
+--bar progress
+bubbles =		"Пузыри",
+
+}
