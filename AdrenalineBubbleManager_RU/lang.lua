209dd3a1ab64b982dfc88d8599081483f864a745
manwithnoface <anbarsukov@gmail.com>
2017-10-23 11:11:24 +0300

    Update RU translation for 4.02

--- AdrenalineBubbleManager_RU/lang.lua
+++ AdrenalineBubbleManager_RU/lang.lua
@@ -1,59 +1,68 @@
---Русификатор для AdrenalineBubbleManager 4.01 by lupus
+--Русификатор для AdrenalineBubbleManager 4.02 by lupus
 --Texts (only text in " ")
 strings = {
 
 --script.lua
-unsafe =		"Необходимо активировать UNSAFE MODE для Homebrew!!!",
-adrinst =		"Плагин AdrBubbleBooter установлен. \nТребуется выполнить перезагрузку PS Vita... :)",
-notadr =		"Adrenaline v6 не обнаружен...",
+unsafe =        "Необходимо активировать UNSAFE MODE для Homebrew!!!",
+adrinst =       "Плагин AdrBubbleBooter установлен. \nТребуется выполнить перезагрузку PS Vita... :)",
+notadr =        "Adrenaline v6 не обнаружен...",
 
 --callbacks.lua
-instbb =		"Создание пузыря: ",
-file =			"Файл: ",
+instbb =        "Создание пузыря: ",
+file =          "Файл: ",
 
 --bubbles.lua
-btitle =		"НАСТРОЙКИ DATA/BOOT.INF",
-count =			"Всего: ",
+btitle =        "МЕНЮ НАСТРОЕК DATA/BOOT.INF",
+count =	        "Всего: ",
 
 --osk
-titleosk =		"Название пузыря",
-putnameosk =	"Введите имя",
+titleosk =      "Название пузыря",
+putnameosk =    "Введите имя",
 --osk
 
-convert =		"Конвертирование ресурсов...",
-errinst =		"В процессе установки произошла ошибка.",
-unk =			"UNK",
+convert =       "Конвертирование ресурсов...",
+errinst =       "В процессе установки произошла ошибка.",
+unk =           "UNK",
 
-marks =			"Select/Start (Выделить/Выделить все)",
-uninstall =		"Удалить",
-editboot =		"Редактировать boot.inf",
-back =			"Назад",
-toggle =		"Изменить значение",
-doneedit =		"Применить",
-notbubbles =	"Данные не найдены.",
-createbb =		"Создайте пузырь и повторите попытку.",
-togoback =		"Назад",
-uninstallbb =	"Будет удалено пузырей: ",
+marks =         "Select/Start (Выделить/Выделить все)",
+uninstall =     "Удалить",
+editboot =      "Редактировать boot.inf",
+back =          "Назад",
+toggle =        "Изменить значение",
+doneedit =      "Применить",
+notbubbles =    "Данные не найдены.",
+createbb =      "Создайте пузырь и повторите попытку.",
+togoback =      "Назад",
+uninstallbb =   "Будет удалено пузырей: ",
+
+--custom message
+title_msg =     "Системное сообщение",
+option_msg =    "Ок",
+option1_msg =   "Да",
+option2_msg =   "Нет",
 
 --scan.lua
-scantitle =		"Доступные игры [ISO/CSO/PBP]",
+scantitle =     "Доступные игры [ISO/CSO/PBP]",
 
 --debug
-loadciso =		"Загрузка C/ISO",
-loadpbp =		"Загрузка PBP",
+loadciso =      "Загрузка C/ISO",
+loadpbp =       "Загрузка PBP",
 --debug
 
-defcolor =		"Фоновый цвет пузыря",
-bbcolor =		"Фоновый цвет пузыря",
-pics =			"Просмотр оформления",
-markgame =		"Выделить/снять выделение",
-bsettings =		"Настройки пузырей",
+fullbbicon =    "Иконка: Растянуть",
+nostretched =   "Иконка: Оригинал",
 
-notpsp =		"Не найдено ни одной PSP игры...",
-installgames =	"Поместите образы iso/cso в папку pspemu/ISO.",
-installgames2 =	"Игры формата eboot.pbp - в pspemu/PSP/GAME/(MYGAME)",
+defcolor =      "Фоновый цвет пузыря",
+bbcolor =       "Фоновый цвет пузыря",
+pics =          "Просмотр оформления",
+markgame =      "Выделить/снять выделение",
+bsettings =     "Настройки пузырей",
+
+notpsp =        "Не найдено ни одной PSP игры...",
+installgames =  "Поместите образы iso/cso в папку pspemu/ISO",
+installgames2 = "Игры формата eboot.pbp - в pspemu/PSP/GAME/(MYGAME)",
 
 --bar progress
-bubbles =		"Пузыри",
+bubbles =       "Пузыри",
 
 }
