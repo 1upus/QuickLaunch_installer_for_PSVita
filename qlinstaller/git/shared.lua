96d034578e8007d0b89ef9ee5ad6530490a7a3fe
manwithnoface <anbarsukov@gmail.com>
2017-10-30 13:15:15 +0300

    update for v0.2

--- qlinstaller/git/shared.lua
+++ qlinstaller/git/shared.lua
@@ -1,9 +1,10 @@
 -- Constants
 
-APP_REPO = "ONElua"
-APP_PROJECT = "Easy-Power-Refresh"
+APP_REPO = "1upus"
+APP_PROJECT = "QuickLaunch_installer_for_PSVita
+"
 
-APP_VERSION_MAJOR = 0x01 -- major.minor
-APP_VERSION_MINOR = 0x00
+APP_VERSION_MAJOR = 0x00 -- major.minor
+APP_VERSION_MINOR = 0x02
 
 APP_VERSION = ((APP_VERSION_MAJOR << 0x18) | (APP_VERSION_MINOR << 0x10)) -- Union Binary
