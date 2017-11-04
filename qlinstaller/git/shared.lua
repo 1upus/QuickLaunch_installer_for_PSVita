4da4f37cce3ff3799e00b9733eb6db9bc078edd1
manwithnoface <anbarsukov@gmail.com>
2017-11-04 19:51:42 +0300

    update v.03

--- qlinstaller/git/shared.lua
+++ qlinstaller/git/shared.lua
@@ -1,10 +1,9 @@
 -- Constants
 
 APP_REPO = "1upus"
-APP_PROJECT = "QuickLaunch_installer_for_PSVita
-"
+APP_PROJECT = "QuickLaunch_installer_for_PSVita"
 
 APP_VERSION_MAJOR = 0x00 -- major.minor
-APP_VERSION_MINOR = 0x02
+APP_VERSION_MINOR = 0x03
 
 APP_VERSION = ((APP_VERSION_MAJOR << 0x18) | (APP_VERSION_MINOR << 0x10)) -- Union Binary
